47bb7f1a4adcc1e9b813274e798fecbe5ea9ee2b
mirmohsen <mirmohsenmousaviwork1367@gmai.com>
2025-02-21 22:23:24 +0330

    feat: delete create_image_media func from helper_func.py

--- app/helpers/helper_func.py
+++ app/helpers/helper_func.py
@@ -1,9 +1,5 @@
 from fastapi.exceptions import HTTPException
 from passlib.context import CryptContext
-import uuid
-from ..config import BASE_DIR
-from pathlib import Path
-import shutil
 
 
 def get_or_404(db, model, id):
@@ -22,12 +18,3 @@ def get_hashed_password(password: str) -> str:
 
 def verify_password(password: str, hashed_pass: str) -> bool:
     return password_context.verify(password, hashed_pass)
-
-
-def create_image_media(folder_name, image):
-    object_folder = BASE_DIR / "media" / f"{folder_name}"
-    object_folder.mkdir(parents=True, exist_ok=True)
-    unique_filename = f"{uuid.uuid4()}{Path(image.filename).suffix}"
-    file_location = object_folder / unique_filename
-    with open(file_location, "wb") as buffer:
-        shutil.copyfileobj(image.file, buffer)
